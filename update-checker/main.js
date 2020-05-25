50b24a5c4489108848bc2ed985bff674948eddb8
Kazanami <dummy.github.1945@gmail.com>
2020-05-25 20:26:33 +0900

    調整

--- update-checker/main.js
+++ update-checker/main.js
@@ -1,6 +1,14 @@
 const fetch = require("node-fetch")
 const fs = require("fs-extra")
+var regexp_url = /((h?)(ttps?:\/\/[a-zA-Z0-9.\-_@:/~?%&;=+#',()*!]+))/g; // ']))/;
+
+let manifest = fs.readFileSync("../bucket/rlogin.json")
 
 fetch("https://api.github.com/repos/kmiya-culti/RLogin/releases")
   .then(res => res.json())
-  .then(json => console.log(json));
+  .then(json =>{
+     let version_URL = json[0].body.match(regexp_url).find((index) => {
+       if (index.match("x64")) return true
+     }).replace(")", "")
+
+  });
